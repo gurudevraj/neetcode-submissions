e7eee0434577beffe747ca203d1079a3cf25995d
gurudevraj <41733999+gurudevraj@users.noreply.github.com>
2026-07-13 22:41:32 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.js
@@ -0,0 +1,54 @@
+class MinStack {
+    constructor() {
+        this.stack = []
+        this.min = null
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        if (this.stack.length === 0) {
+            this.min = val
+        } else {
+            if (this.min > val) {
+                this.min = val
+            }
+        }
+        this.stack.push(val)
+        return this.stack
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        const len = this.stack.length
+        if (len === 0) return null
+        if (this.min === this.top()) {
+            const temp = this.stack[0]
+            for (let i = 0; i < len - 2; i++) {
+                if (temp > this.stack[i]) temp = this.stack[i]
+            }
+            this.min = temp
+        }
+        return this.stack.pop()
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        const len = this.stack.length
+        if (len === 0) return null
+        return this.stack[len - 1]
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.min
+    }
+}
